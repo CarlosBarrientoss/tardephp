961d3e5a56f3cb982408197847f863609859161f
CarlosBarrientoss <carlosabarrientoss@gmail.com>
2025-06-13 14:30:15 -0500

    inicial

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "app/controladores/plantilla.controlador.php";
+require_once "app/controladores/login.controller.php";
+
+require_once "app/modelos/login.model.php";
+
+$plantilla = new ControladorPlantilla();
+
+$plantilla->ctrPlantilla();
